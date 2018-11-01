c12534b5f5cc0cbb8dcd6d2f8e79a5255656d893
SiapaTauKamiIniAdalahwgkwoe[hkwepgk23g;24 <39037579+FaiqGaming@users.noreply.github.com>
2018-11-01 12:10:33 +0900

    Create shard.js

---
+++ shard.js
@@ -0,0 +1,20 @@
+
+// TUTORIAL SHARDING (discord.js)
+const { ShardingManager } = require('discord.js');
+const TOKEN = `YOUR TOKEN HERE`; 
+const mainFile = "shard.js"; //GANTI INI!
+
+const shards = new ShardingManager(`${mainFile}`, {
+    token: process.env.BOT_TOKEN,
+    totalShards: 1 //1 SAJA DULU ;v
+});
+
+shards.on('launch', shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
+});
+
+shards.on('message', (shard, msg) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
+});
+
+shards.spawn();
