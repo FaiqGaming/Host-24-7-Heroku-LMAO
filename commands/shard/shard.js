76c788c7fd607db046dfaee15d44353cd836bb1b
SiapaTauKamiIniAdalahwgkwoe[hkwepgk23g;24 <39037579+FaiqGaming@users.noreply.github.com>
2018-11-01 12:02:22 +0900

    Update shard.js

--- commands/shard/shard.js
+++ commands/shard/shard.js
@@ -1,8 +1,19 @@
-/*
-    The following code goes into it's own file, and you run this file
-    instead of your main bot file.
-*/
- 
-const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./index.js');
-Manager.spawn(2); // This example will spawn 2 shards (5,000 guilds);
+// TUTORIAL SHARDING (discord.js)
+const { ShardingManager } = require('discord.js');
+const TOKEN = `YOUR TOKEN HERE`; 
+const mainFile = "shard.js"; //GANTI INI!
+
+const shards = new ShardingManager(`${mainFile}`, {
+    token: process.env.BOT_TOKEN,
+    totalShards: 1 //1 SAJA DULU ;v
+});
+
+shards.on('launch', shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
+});
+
+shards.on('message', (shard, msg) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
+});
+
+shards.spawn();
