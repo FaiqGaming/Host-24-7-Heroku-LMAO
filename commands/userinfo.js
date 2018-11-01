4291d0c1698d3267e14289515989bb7f6d7e6af8
SiapaTauKamiIniAdalahwgkwoe[hkwepgk23g;24 <39037579+FaiqGaming@users.noreply.github.com>
2018-11-01 16:10:25 +0900

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,86 +1,39 @@
-//Define discord-js
-const Discord = require('discord.js');
+        const UserInfo = new Discord.MessageEmbed()
 
-//Define moment
-const moment = require("moment");
+            //All Fields are Optional Pick Any some
 
-exports.run = async (client, message, args) => {
-    message.delete()
-	let user;
-	// If the user mentions someone, display their stats. If they just run userinfo without mentions, it will show their own stats.
-    if (message.mentions.users.first()) {
-      user = message.mentions.users.first();
-    } else {
-        user = message.author;
+            .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
+            .setTitle('UserInfo')
+            .setURL('www.google.com') //Any Vaild Link
+            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
+            .setImage(message.author.avatarURL()) //Add Any Image URl || Image
+            .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
+
+            //All Feilds Are Just Examples pick Some & add as you like
+
+            .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
+            .addField('AvatarURL', message.author.avatarURL({
+                format: 'png'
+            }), true) //{options} options are Size?: 128 | 256 | 512 | 1024 | 2048, Format?: "webp" | "png" | "jpg" | "gif" //.defaultAvatarURL() A link to the user's default avatar //.displayAvatarURL() A link to the user's avatar if they have one. Otherwise a link to their default avatar will be returned
+            .addField('AvatarURL', message.author.avatarURL({
+                size: '2048'
+            }), true)
+            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
+            .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
+            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
+            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
+            .addField('ID', message.author.id) //The ID of the User/author
+            .addField('Last Message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
+            .addField('Last Message ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
+            .addField('Presence', message.author.presence) //The presence of this user
+            .addField('Presence Status', message.author.presence.status) //The presence status of this user
+            .addField('Presence Game', message.author.presence.activity.name) //The presence Game of this user
+            .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
+            .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
+            .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
+
+            .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
+            .setTimestamp() //The timestamp of this embed
+
+        message.channel.send(UserInfo);
     }
-	// Define the member of a guild.
-    const member = message.guild.member(user);
-	
-	//Discord rich embed
-                  const embed = new Discord.RichEmbed()
-                        .setAuthor('Support our bot!', client.user.displayAvatarURL)
-                        .setDescription("Here's the special perks:")
-                        .setColor('#FFD800')
-                        .addField(':arrow_forward: Invite bot', `[Click here](${'https://discordapp.com/oauth2/authorize?client_id=481305515092213763&permissions=371776&scope=bot'})`, true)
-                        .addField(':earth_asia: Join my server', `[Click here](${'http://discord.gg/rKjFGps'})`, true)
-                        .addField(':pencil: Contact us', `[Click here](${'https://goo.gl/forms/0km4ybWuVWLQ83T33'})`, true)
-                        .addField('<:YouTube:506769226766483457> My Channel', `[Click here](${'https://www.youtube.com/user/rozivector1999'})`, true)
-                        .addField('<:twitch:506770621498392594> My Streaming', `[Click here](${'https://www.twitch.tv/rozivector'})`, true)
-                        .addField(':earth_asia: My Website', `[Click here](${'http://rozivector.blogspot.com'})`, true)
-                        .setFooter("Now support 24/7 online bot! Made with discord.js")
-                        message.channel.sendMessage(embed)
-                        message.channel.stopTyping(true);
-                    };
-
-        {
-
-            let comrade = message.content.slice(prefix.length).trim().split(' ');
-
-    
-                
-
-                const ColorMap = 
-                {
-                    'online' : '#00FF00',
-                    'idle' : '#FF8000',
-                    'streaming' : '#A901DB',
-                    'dnd' : '#FF0000',
-                    'offline' : '#848484'
-                };
-                const ngebot = 
-                {
-                    'true' : 'Bot User',
-                    'false' : 'Regular User'
-                };
-                const StatusMap = 
-                {
-                    'online' : `<:online:504813930313547776>`,
-                    'idle' : `<:idle:504813930321805333>`,
-                    'streaming' : `<:streaming:504813930309222400>`,
-                    'offline' : `<:offline:504813929780871191>`,
-                    'dnd' : `<:dnd:504813930246438912>`
-
-                };
-
-                const StatusText = 
-                {
-                    'online' : 'Online',
-                    'idle' : 'Idle',
-                    'dnd' : 'Do Not Disturb',
-                    'offline' : 'Offline',
-                    'streaming' : 'Streaming'
-                } 
-
-                const verlev = 
-                {
-                    '0' : 'None',
-                    '1' : 'Low',
-                    '2' : 'Medium',
-                    '3' : 'High',
-                    '4' : 'Very High'
-
-}
-
-exports.help = {
-    name: "userinfo"
-}
