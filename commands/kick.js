f9f4fd301a725685db02208871f629844666cdd7
FaiqGaming <39037579+FaiqGaming@users.noreply.github.com>
2018-11-01 09:38:09 +0900

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission("KICK_MEMBERS")) return errors.noPerms(message, "KICK_MEMBERS");
+    if(args[0] == "help"){
+      message.reply("Usage: !kick <user> <reason>");
+      return;
+    }
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return errors.cantfindUser(message.channel);
+    let kReason = args.join(" ").slice(22);
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, kUser, "MANAGE_MESSAGES");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("Kick Removed Logs")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "💬server▪log💬");
+    if(!kickChannel) return message.channel.send("Cannot Find 💬server▪log💬 Channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    message.channel.send(`${kUser} Has Been Kicked By ${message.author} Reason: ${kReason}`);
+    kickChannel.send(kickEmbed);
+}
+
+module.exports.help = {
+  name:"kick"
+}
