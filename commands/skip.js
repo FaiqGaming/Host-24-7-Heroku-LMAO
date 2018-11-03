45fde8005775da44ea1e0b7bf8a0dbc6d9df1110
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-11-03 14:00:15 +0900

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,94 +1,46 @@
-const cfg = require("../config.json");
-const Discord = require("discord.js");
-const key = process.env.SERVER_YOUTUBE;
-const fs = require("fs"); 
-const moment = require('moment');
-const yt = require('ytdl-core');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube(key);
-const opus = require("opusscript");
-const gyp = require("node-gyp");
+// you can use your own command handler this doesn't require any depencies
+exports.run = async (client, message, args, ops) => {
 
-exports.run = async(message, args, color, queue) => { 
-    const args1 = message.content.split(' ');
-	const searchString = args1.slice(1).join(' ');
-	const url = args1[1] ? args1[1].replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = queue.get(message.guild.id);
+    message.delete()
+    // First, we need to fetch the guild object
+    let fetched = ops.active.get(message.guild.id);
 
-if (!message.member.voiceChannel) return message.channel.send('You are not in a voice channel!');
-		if (!serverQueue) return message.channel.send("I can't skip Nothing?");
-      		serverQueue.connection.dispatcher.end('Skip command has been used!');
-		return undefined;
+    // Then, we need to check if what we fetched is defined
+    if (!fetched) return message.channel.send('There currently isn\'t any music in the guild');
 
-    // Time for the functions
-    async function handleVideo(video, message, voiceChannel, playlist = false) {
-  const serverQueue = queue.get(message.guild.id);
-  console.log(video);
-  const song = {
-    id: video.id,
-    title: video.title,
-    url: `https://www.youtube.com/watch?v=${video.id}`
-  };
-  if (!serverQueue) {
-    const queueConstruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      skippers: [],
-      songs: [],
-      volume: 50,
-      playing: true
-    };
-    queue.set(message.guild.id, queueConstruct);
+    // Check if the user is in the same channel as the bot
+    if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send('Sorry you currently aren\'t in the same channel as the bot!');
 
-    queueConstruct.songs.push(song);
+    // Fetch how many users are in the channel
+    let userCount = message.member.voiceChannel.members.size;
 
-    try {
-      var connection = await voiceChannel.join();
-      queueConstruct.connection = connection;
-      play(message.guild, queueConstruct.songs[0]);
-    } catch (error) {
-      console.error(`I could not join the voice channel: ${error}`);
-      queue.delete(message.guild.id);
-      return message.channel.send(`I could not join the voice channel: ${error}`);
+    // Calculate how many votes are required to skip
+    let required = Math.ceil(userCount/2);
+
+    // Update Fetched
+    if (!fetched.queue[0].voteSkips) fetched.queue[0].voteSkips = [];
+
+    // Check if the user already voted to skip
+    if (fetched.queue[0].voteSkips.includes(message.member.id)) return message.channel.send(`Sorry, you already voted to skip! ${fetched.queue[0].voteSkips.length}/${required} required.`);
+
+    // Add the user to voteSkips
+    fetched.queue[0].voteSkips.push(message.member.id);
+
+    // Update Map
+    ops.active.set(message.guild.id, fetched);
+
+    // Check if there is enough votes to skip
+    if (fetched.queue[0].voteSkips.length >= required) {
+
+        // Send output
+        message.channel.send("Succesfully skipped song!");
+
+        // emit finish event & return
+        return fetched.dispatcher.emit('finish');
+
     }
-  } else {
-    serverQueue.songs.push(song);
-    console.log(serverQueue.songs);
-    if (playlist) return undefined;
-    else return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
-  }
-  return undefined;
-}
 
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
+    // Otherwise, tell them in chat that they added a vote to skip
+    message.channel.send(`Succesfully voted to skip! ${fetched.queue[0].voteSkips.length}/${required} required`);
 
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
-  console.log(serverQueue.songs);
-
-const dispatcher = serverQueue.connection.playStream(yt(song.url))
-        .on('end', reason => {
-            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
-            else console.log(reason);
-            serverQueue.songs.shift();
-            setTimeout(() => {
-                play(guild, serverQueue.songs[0]);
-            }, 250);
-        })
-        .on('error', error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 50);
-
-    serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
-}
-}
-
-exports.help = {
-    name: "skip",
-    description: "Skips the current song thats playing!",
-    usage: "(skip"
-}
+} // Now, we can test it!
