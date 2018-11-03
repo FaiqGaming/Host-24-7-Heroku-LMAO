3944bc86f707ebe8e3e994ba01ca4b8684f18e11
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-11-03 14:01:06 +0900

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -1,104 +1,25 @@
-const cfg = require("../config.json");
-const Discord = require("discord.js");
-const key = process.env.YT_API;
-const fs = require("fs"); 
-const moment = require('moment');
-const yt = require('ytdl-core');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube(key);
-const opus = require("opusscript");
-const gyp = require("node-gyp");
+// This command doesn't require any modules
+exports.run = (client, message, args, ops) => {
 
-exports.run = async(music, message, args, color, queue) => { 
-  const args1 = message.content.split(' ');
-  const searchString = args1.slice(1).join(' ');
-  const url = args1[1] ? args1[1].replace(/<(.+)>/g, '$1') : '';
-  const serverQueue = queue.get(message.guild.id); 
- 
-    if (serverQueue && !serverQueue.playing) {
-      serverQueue.playing = true;
-      serverQueue.connection.dispatcher.resume();
-      
-      const rEmbed = new Discord.RichEmbed()
-      .setDescription(":arrow_forward: Resume the paused music for you!")
-      .setColor(color);
+    message.delete()
+    // First, we need to fetch the guild object
+    let fetched = ops.active.get(message.guild.id);
 
-      return message.channel.send(rEmbed);
-    }
-    return message.channel.send("There isn't anything playing for me to resume!");
- 
-    return undefined;
+    // Then, we need to check if what was fetched is defined
+    if (!fetched) return message.channel.send('There currently isn\'t any music playing in this guild!');
 
-    // Time for the functions
+    // Check if the user is in the same channel as the bot
+    if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send('Sorry, you aren\'t in the same channel as the music bot.');
 
-async function handleVideo(video, message, voiceChannel, playlist = false) {
-  const serverQueue = queue.get(message.guild.id);
-  console.log(video);
-  const song = {
-    id: video.id,
-    title: video.title,
-    url: `https://www.youtube.com/watch?v=${video.id}`
-  };
-  if (!serverQueue) {
-    const queueConstruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      skippers: [],
-      songs: [],
-      volume: 5,
-      playing: true
-    };
-    queue.set(message.guild.id, queueConstruct);
+    // Then, check if the dispatcher is already paused
+    if (!fetched.dispatcher.paused) return message.channel.send('This music isn\'t paused.');
 
-    queueConstruct.songs.push(song);
+    // Finally, if it hasn't returned yet, resume the music
+    fetched.dispatcher.resume();
 
-    try {
-      var connection = await voiceChannel.join();
-      queueConstruct.connection = connection;
-      play(message.guild, queueConstruct.songs[0]);
-    } catch (error) {
-      console.error(`I could not join the voice channel: ${error}`);
-      queue.delete(message.guild.id);
-      return message.channel.send(`I could not join the voice channel: ${error}`);
-    }
-  } else {
-    serverQueue.songs.push(song);
-    console.log(serverQueue.songs);
-    if (playlist) return undefined;
-    else return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
-  }
-  return undefined;
-}
+    // Send Output
+    message.channel.send(`Successfully resumed ${fetched.queue[0].songTitle}`);
 
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
+} // To convert it, we have change a few lines.
 
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
-  console.log(serverQueue.songs);
-
-const dispatcher = serverQueue.connection.playStream(yt(song.url))
-        .on('end', reason => {
-            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
-            else console.log(reason);
-            serverQueue.songs.shift();
-            setTimeout(() => {
-                play(guild, serverQueue.songs[0]);
-            }, 250);
-        })
-        .on('error', error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-
-    serverQueue.textChannel.send(`🎶 Now playing: **${song.title}**`);
-}
-}
-
-exports.help = {
-  name: "resume",
-  description: "resume the paused music",
-usage: "(resume"
-}
+// Now, we can test the two commands!
