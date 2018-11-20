743bbae54d85e7b4b9b45bfb318e909adc7673be
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-11-20 16:05:47 +0900

    Update ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -3,10 +3,13 @@ const {token} = require('../settiings/credentials.json');
 module.exports = {
 
     ready : (bot) => {
-        bot.login(token)
-        bot.on('ready', () => {
-            client.user.setActivity('', {type: 'LISTENING'});
-            client.user.setStatus('dnd');
+        client.login(process.env.BOT_TOKEN)
+        client.on('ready', () => {
+function changing_status() {
+  let status = [`Say (help || Released Now!`, `With ${client.users.size} Users`, `With ${client.guilds.size} Server`, `With ${client.channels.size} Channels`, `24/7 Online Forever`, `Hosted In Heroku`]
+  let random = status[Math.floor(Math.random() * status.length)]
+  client.user.setActivity(random)
+}
             console.log('I am ready to play MUSICS!!');
         });
     }
